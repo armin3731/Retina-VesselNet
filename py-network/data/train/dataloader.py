f069dcc22a8947cca4ea98dfa760f84fbd29d1be
Armin Eskandarinasab <armin3731@live.com>
2025-05-12 14:25:02 +0200

    fix: train.py is working now

--- py-network/data/train/dataloader.py
+++ py-network/data/train/dataloader.py
@@ -10,13 +10,15 @@ from tqdm import tqdm
 from sklearn.utils import shuffle
 from data.train.image2patch import image2patch
 from util.load_cfg import sample_cfg
+import kagglehub
 
 
-patch_size=sample_cfg["patch_size"]
-patch_threshold=sample_cfg["patch_threshold"]
-patch_num=sample_cfg["patch_num"]
-patch_threshold=sample_cfg["patch_threshold"]
-train_val_rate=sample_cfg["train_val_rate"]
+patch_size = sample_cfg["patch_size"]
+patch_threshold = sample_cfg["patch_threshold"]
+patch_num = sample_cfg["patch_num"]
+patch_threshold = sample_cfg["patch_threshold"]
+train_val_rate = sample_cfg["train_val_rate"]
+
 
 def load_image_groundtruth(img_path, groundtruth_path):
     img = tf.io.read_file(img_path)
@@ -46,65 +48,113 @@ def load_image_groundtruth(img_path, groundtruth_path):
     return img, groundtruth
 
 
-
-def get_dataset(dataset_cfg,regenerate=True):
+def get_dataset(dataset_cfg, regenerate=True):
     ## load configs
-    dataset_path = dataset_cfg["dataset_path"]  # modify the dataset_path to your own dir（将dataset_path修改至你自己的路径）
-    train_dir = dataset_path + dataset_cfg["train_dir"]
-    train_image_dir = train_dir + dataset_cfg["train_image_dir"]
-    train_mask_dir = train_dir + dataset_cfg["train_mask_dir"]
-    train_groundtruth_dir = train_dir + dataset_cfg["train_groundtruth_dir"]
-    train_patch_dir = train_dir + dataset_cfg["train_patch_dir"]
+    dataset_path = kagglehub.dataset_download(dataset_cfg["dataset_path"])
+    # modify the dataset_path to your own dir（将dataset_path修改至你自己的路径）
+    train_dir = os.path.join(dataset_path, dataset_cfg["train_dir"])
+    train_image_dir = os.path.join(train_dir, dataset_cfg["train_image_dir"])
+    train_mask_dir = os.path.join(train_dir, dataset_cfg["train_mask_dir"])
+    train_groundtruth_dir = os.path.join(
+        train_dir, dataset_cfg["train_groundtruth_dir"]
+    )
+    train_patch_dir = os.path.join(train_dir, dataset_cfg["train_patch_dir"])
 
     # scan the training image
-    train_image_path_list = glob(train_image_dir + "*.tif")
+    train_image_path_list = glob(os.path.join(train_image_dir, "*.tif"))
 
-    #split training/val set
-    val_image_path_list = random.sample(train_image_path_list, int(len(train_image_path_list) * (1 - train_val_rate)))
-    train_image_path_list = [i for i in train_image_path_list if i not in val_image_path_list]
+    # split training/val set
+    val_image_path_list = random.sample(
+        train_image_path_list, int(len(train_image_path_list) * (1 - train_val_rate))
+    )
+    train_image_path_list = [
+        i for i in train_image_path_list if i not in val_image_path_list
+    ]
 
     print("[INFO] Number of training images:", len(train_image_path_list))
     print("[INFO] Number of valid images:", len(val_image_path_list))
 
     if not os.path.exists(train_patch_dir):
         os.mkdir(train_patch_dir)
-    
+
     # generate patch images (生成图像块数据)
     if regenerate:
         shutil.rmtree(train_patch_dir)
         os.mkdir(train_patch_dir)
-        
-        for i in tqdm(range(len(train_image_path_list)), desc="Generate the training patches: "):
-            image2patch(train_image_path_list[i], patch_num, patch_size,patch_threshold,train_patch_dir,train_groundtruth_dir,train_mask_dir, training=True,show=False)  # set show=True to visualize the sample process, which is much slower than show=False
 
-        for i in tqdm(range(len(val_image_path_list)), desc="Generate the val patches: "):
-            image2patch(val_image_path_list[i], patch_num, patch_size,patch_threshold,train_patch_dir,train_groundtruth_dir,train_mask_dir, training=False,show=False)  # set show=True to visualize the sample process, which is much slower than show=False
+        for i in tqdm(
+            range(len(train_image_path_list)), desc="Generate the training patches: "
+        ):
+            image2patch(
+                train_image_path_list[i],
+                patch_num,
+                patch_size,
+                patch_threshold,
+                train_patch_dir,
+                train_groundtruth_dir,
+                train_mask_dir,
+                training=True,
+                show=False,
+            )  # set show=True to visualize the sample process, which is much slower than show=False
+
+        for i in tqdm(
+            range(len(val_image_path_list)), desc="Generate the val patches: "
+        ):
+            image2patch(
+                val_image_path_list[i],
+                patch_num,
+                patch_size,
+                patch_threshold,
+                train_patch_dir,
+                train_groundtruth_dir,
+                train_mask_dir,
+                training=False,
+                show=False,
+            )  # set show=True to visualize the sample process, which is much slower than show=False
     else:
         print("[INFO] use generated patches...")
     # load training/val patches
-    train_patch_img_path_list = sorted(glob(train_patch_dir + "*-*-img.jpg"))
-    train_patch_groundtruth_path_list = sorted(glob(train_patch_dir + "*-*-groundtruth.jpg"))
-    train_patch_img_path_list, train_patch_groundtruth_path_list = shuffle(train_patch_img_path_list,train_patch_groundtruth_path_list,random_state=0)
+    train_patch_img_path_list = sorted(
+        glob(os.path.join(train_patch_dir, "*-*-img.jpg"))
+    )
+    train_patch_groundtruth_path_list = sorted(
+        glob(os.path.join(train_patch_dir, "*-*-groundtruth.jpg"))
+    )
+    train_patch_img_path_list, train_patch_groundtruth_path_list = shuffle(
+        train_patch_img_path_list, train_patch_groundtruth_path_list, random_state=0
+    )
 
     # make sure that img-list and mask-list is in order (确保打乱后的image-mask还是对应的)
     # print(len(train_patch_img_path_list), len(train_patch_groundtruth_path_list))
     # print(train_patch_img_path_list[:2])
     # print(train_patch_groundtruth_path_list[:2])
 
-    val_patch_img_path_list = sorted(glob(train_patch_dir + "*_*_val_img.jpg"))
-    val_patch_groundtruth_path_list = sorted(glob(train_patch_dir + "*_*_val_groundtruth.jpg"))
+    val_patch_img_path_list = sorted(
+        glob(os.path.join(train_patch_dir, "*_*_val_img.jpg"))
+    )
+    val_patch_groundtruth_path_list = sorted(
+        glob(os.path.join(train_patch_dir, "*_*_val_groundtruth.jpg"))
+    )
 
     # print(val_patch_img_path_list[:2])
     # print(val_patch_groundtruth_path_list[:2])
 
     # Training Dataloader
-    train_dataset = tf.data.Dataset.from_tensor_slices((train_patch_img_path_list, train_patch_groundtruth_path_list))
-    train_dataset = train_dataset.map(load_image_groundtruth, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    #train_dataset = train_dataset.shuffle(buffer_size=1300).prefetch(BATCH_SIZE).batch(BATCH_SIZE)
+    train_dataset = tf.data.Dataset.from_tensor_slices(
+        (train_patch_img_path_list, train_patch_groundtruth_path_list)
+    )
+    train_dataset = train_dataset.map(
+        load_image_groundtruth, num_parallel_calls=tf.data.experimental.AUTOTUNE
+    )
+    # train_dataset = train_dataset.shuffle(buffer_size=1300).prefetch(BATCH_SIZE).batch(BATCH_SIZE)
 
     # VAL Dataloader
-    val_dataset = tf.data.Dataset.from_tensor_slices((val_patch_img_path_list, val_patch_groundtruth_path_list))
-    val_dataset = val_dataset.map(load_image_groundtruth, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    #val_dataset = val_dataset.shuffle(buffer_size=1300).prefetch(BATCH_SIZE).batch(BATCH_SIZE)
+    val_dataset = tf.data.Dataset.from_tensor_slices(
+        (val_patch_img_path_list, val_patch_groundtruth_path_list)
+    )
+    val_dataset = val_dataset.map(
+        load_image_groundtruth, num_parallel_calls=tf.data.experimental.AUTOTUNE
+    )
+    # val_dataset = val_dataset.shuffle(buffer_size=1300).prefetch(BATCH_SIZE).batch(BATCH_SIZE)
 
-    return train_dataset,val_dataset
+    return train_dataset, val_dataset
--- py-network/train.py
+++ py-network/train.py
@@ -1,8 +1,10 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = "1"  # use GPU-0
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # use GPU-0
 
 import tensorflow as tf
-gpus = tf.config.list_physical_devices('GPU')
+
+gpus = tf.config.list_physical_devices("GPU")
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
@@ -12,8 +14,8 @@ from util.dice import *
 from model.unet import Unet
 from data.train import dataloader
 
-checkpoint_dir=train_cfg["checkpoint_dir"]
-log_dir=train_cfg["log_dir"]
+checkpoint_dir = train_cfg["checkpoint_dir"]
+log_dir = train_cfg["log_dir"]
 
 if not os.path.exists(checkpoint_dir):
     os.mkdir(checkpoint_dir)
@@ -21,30 +23,36 @@ if not os.path.exists(checkpoint_dir):
 if not os.path.exists(log_dir):
     os.mkdir(log_dir)
 
-model=Unet(sample_cfg["patch_size"])
+model = Unet(sample_cfg["patch_size"])
 
 # Learning rate and optimizer （学习率调整和优化器）
-cosine_decay = tf.keras.experimental.CosineDecayRestarts(initial_learning_rate=train_cfg["init_lr"], first_decay_steps=12000,t_mul=1000,m_mul=0.5,alpha=1e-5)
-optimizer=tf.keras.optimizers.Adam(learning_rate=cosine_decay)
+cosine_decay = tf.keras.optimizers.schedules.CosineDecayRestarts(
+    initial_learning_rate=train_cfg["init_lr"],
+    first_decay_steps=12000,
+    t_mul=1000,
+    m_mul=0.5,
+    alpha=1e-5,
+)
+optimizer = tf.keras.optimizers.Adam(learning_rate=cosine_decay)
 
 # loss function （损失函数）
-#loss=tf.keras.losses.BinaryCrossentropy(from_logits=False)
+# loss=tf.keras.losses.BinaryCrossentropy(from_logits=False)
 
 # metric record （性能指标记录器）
-train_loss = tf.keras.metrics.Mean(name='train_loss')
-train_acc=tf.keras.metrics.Mean(name='train_acc')
-current_accuracy = tf.keras.metrics.BinaryAccuracy(name='train_accuracy')
+train_loss = tf.keras.metrics.Mean(name="train_loss")
+train_acc = tf.keras.metrics.Mean(name="train_acc")
+current_accuracy = tf.keras.metrics.BinaryAccuracy(name="train_accuracy")
 
-val_loss = tf.keras.metrics.Mean(name='val_loss')
-val_acc=tf.keras.metrics.Mean(name='val_acc')
-val_accuracy = tf.keras.metrics.BinaryAccuracy(name='val_accuracy')
+val_loss = tf.keras.metrics.Mean(name="val_loss")
+val_acc = tf.keras.metrics.Mean(name="val_acc")
+val_accuracy = tf.keras.metrics.BinaryAccuracy(name="val_accuracy")
 
 # checkpoint （模型存档管理器）
 ckpt = tf.train.Checkpoint(model=model)
-#ckpt.restore(tf.train.latest_checkpoint(checkpoint_path))
+# ckpt.restore(tf.train.latest_checkpoint(checkpoint_path))
 
 # tensorboard writer （Tensorboard记录器）
-log_dir=log_dir+ datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+log_dir = log_dir + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 log_writer = tf.summary.create_file_writer(log_dir)
 
 
@@ -77,26 +85,34 @@ def val_step(step, patch, groundtruth):
     tf.summary.image("pred", pred_seg, step=step)
     log_writer.flush()
 
-def train_function(train_dataset,val_dataset):
+
+def train_function(train_dataset, val_dataset):
     lr_step = 0
     last_val_loss = 2e10
-    EPOCHS=train_cfg["epoch"]
-    VAL_TIME=train_cfg["val_time"]
+    EPOCHS = train_cfg["epoch"]
+    VAL_TIME = train_cfg["val_time"]
 
     with log_writer.as_default():
         for epoch in range(EPOCHS):
             # renew the recorder （重置记录项）
-            train_loss.reset_states()
-            train_acc.reset_states()
-            val_loss.reset_states()
-            val_acc.reset_states()
+            train_loss.reset_state()
+            train_acc.reset_state()
+            val_loss.reset_state()
+            val_acc.reset_state()
 
             # training （训练部分）
             for tstep, (patch, groundtruth) in enumerate(train_dataset):
                 train_step(lr_step, patch, groundtruth)
 
-                tf.summary.scalar("learning_rate", optimizer._decayed_lr(tf.float32).numpy(), step=lr_step)
-                print('\repoch {}, batch {}, loss:{:.4f}, dice:{:.4f}'.format(epoch + 1, tstep, train_loss.result(),train_acc.result()), end="")
+                tf.summary.scalar(
+                    "learning_rate", optimizer.learning_rate.numpy(), step=lr_step
+                )
+                print(
+                    "\repoch {}, batch {}, loss:{:.4f}, dice:{:.4f}".format(
+                        epoch + 1, tstep, train_loss.result(), train_acc.result()
+                    ),
+                    end="",
+                )
                 lr_step += 1
 
             if (epoch + 1) % VAL_TIME == 0:
@@ -104,7 +120,17 @@ def train_function(train_dataset,val_dataset):
                 for vstep, (patch, groundtruth) in enumerate(val_dataset):
                     val_step(lr_step, patch, groundtruth)
 
-                print('\repoch {}, batch {}, train_loss:{:.4f}, train_dice:{:.4f}, val_loss:{:.4f}, val_dice:{:.4f}'.format(epoch + 1, vstep, train_loss.result(), train_acc.result(), val_loss.result(), val_acc.result()),end="")
+                print(
+                    "\repoch {}, batch {}, train_loss:{:.4f}, train_dice:{:.4f}, val_loss:{:.4f}, val_dice:{:.4f}".format(
+                        epoch + 1,
+                        vstep,
+                        train_loss.result(),
+                        train_acc.result(),
+                        val_loss.result(),
+                        val_acc.result(),
+                    ),
+                    end="",
+                )
                 tf.summary.scalar("val_loss", val_loss.result(), step=epoch)
                 tf.summary.scalar("val_dice", val_acc.result(), step=epoch)
 
@@ -118,10 +144,17 @@ def train_function(train_dataset,val_dataset):
 
 
 if __name__ == "__main__":
+    train_dataset, val_dataset = dataloader.get_dataset(dataset_cfg, regenerate=True)
 
-    train_dataset,val_dataset=dataloader.get_dataset(dataset_cfg,regenerate=True)
+    train_dataset = (
+        train_dataset.shuffle(buffer_size=1300)
+        .prefetch(train_cfg["batch_size"])
+        .batch(train_cfg["batch_size"])
+    )
+    val_dataset = (
+        val_dataset.shuffle(buffer_size=1300)
+        .prefetch(train_cfg["batch_size"])
+        .batch(train_cfg["batch_size"])
+    )
 
-    train_dataset = train_dataset.shuffle(buffer_size=1300).prefetch(train_cfg["batch_size"]).batch(train_cfg["batch_size"])
-    val_dataset = val_dataset.shuffle(buffer_size=1300).prefetch(train_cfg["batch_size"]).batch(train_cfg["batch_size"])
-
-    train_function(train_dataset,val_dataset)
+    train_function(train_dataset, val_dataset)
